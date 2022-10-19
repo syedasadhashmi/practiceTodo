9173e78c612ce7929a649cd278863000d02f70be
unknown <asadhashmi22@gmail.com>
2022-10-19 14:04:37 +0500

    details page task completed

--- src/component/Details.js
+++ src/component/Details.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-const Details = () => {
-  const params = useParams();
-  const id = params.id;
-  return <div>Details {id}</div>;
+const Details = ({ todoData }) => {
+  const { id } = useParams();
+  //   const id = params.id;
+  const [details, setDetails] = useState([]);
+
+  useEffect(() => {
+    const getData = JSON.parse(localStorage.getItem('todos'));
+    console.log(getData);
+    setDetails(getData);
+  }, []);
+  console.log(JSON.parse(localStorage.getItem('todos')));
+  return (
+    <div>
+      Details {id}
+      {details?.map(item => {
+        if (item.id === id) {
+          return (
+            <div key={item.id}>
+              <h4>Todo Title: {item.title}</h4>
+              <h4>Todo Description: {item.description}</h4>
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
 };
 
 export default Details;
